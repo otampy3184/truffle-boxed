bf20cba8f215c16e69bf0a12bbcb16421e4dd467
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-01 17:30:21 +0900

    utilsにMathを追加

---
+++ contracts/utils/math/Math.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+library Math {
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256){
+        return a < b ? a : b;
+    }
+
+    function average(uint256 a, uint256 b) internal pure returns (uint256){
+        return (a & b ) + ( a ^ b) / 2;
+    }
+
+    function ceilDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a/ b + (a % b == 0 ? 0 : 1);
+    }
+}
